f8acfeb7fd059afd5eb9f57f584302aa68bdb845
Stephanie Hickman <stephick@gmail.com>
2024-02-01 13:36:15 -0600

    finished updating GET and POST on server.js

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -5,7 +5,7 @@ const pg = require('pg'); // importing pg
 // DB CONNECTION
 
 const pool = new pg.Pool({
-    database:  '<db name>',  //TODO - get db name from keef
+    database:  '<Koala>',  
     host:  'localhost',
     port: 5432
 })
@@ -17,7 +17,7 @@ koalaRouter.get('/', (req, res) => {
     //get koalas from the database
     //write a query a string
 
-    let queryText = `SELECT * FROM "db name"` //TODO - get db name from keef
+    let queryText = `SELECT * FROM "Koala"`
 
     // need to send the query to the database
 
@@ -41,8 +41,52 @@ koalaRouter.get('/', (req, res) => {
 
 // POST
 
+koalaRouter.post('/', (req, res) => {
+//anticipating the req body will look like:
+
+// {name: ,
+//  favorite_color
+//  age:  ,
+//  ready_to_transfer: ,
+//  notes   ,
+// }
+
+console.log (req.body);
+let newKoala = req.body;
+
+
+let queryText = `
+    INSERT INTO "Koala" ("name", "favorite_color", "age", "ready_to_transfer", "notes")
+    VALUES
+    ($1, $2, $3, $4, $5)
+    ;`
+
+//Need to pass the query into the pool, along with a second parameter
+// The second parameter is the list of things we want pg to safely put into the template
+
+pool.query(queryText, [newKoala.name, newKoala.favorite_color, newKoala.age, newKoala.ready_to_transfer, newKoala.notes])
+    .then(
+        (result) => {
+            console.log(`this post query worked, ${queryText}`, result);
+            res.sendStatus(201);
+        }
+    ) 
+    .catch(
+        (error) => {
+            console.log (`this post query failed, ${queryText}`, error);
+            res.sendStatus(500)
+        }
+    );
+
+});
 
 // PUT
+
+// koalaRouter.put ('/mark/:id', (req, res) =>
+
+//     console.log('in the put on the server');
+    
+//     )
 
 
 // DELETE
